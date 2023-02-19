3ad197fa111f78ec243c4267582a6a67f1d41c8c
asmaa <asmaa@info.com>
2023-02-19 14:57:58 +0200

    add job views&url

--- Job/models.py
+++ Job/models.py
@@ -11,5 +11,10 @@ class Job(models.Model):
     vacancy=models.IntegerField(default=0)
     salary=models.IntegerField(default=1)
     experience=models.IntegerField(default=1)
+    category=models.ForeignKey('Category',on_delete=models.CASCADE)
     def __str__(self):
         return self.title
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
